20d697be5ab5cc918fcee9fe54d7ff11c8d62f54
caijiahong <1376313044@qq.com>
2018-05-31 11:52:04 +0800

    all

---
+++ Hello.java
@@ -0,0 +1,18 @@
+package com.jike.po.audit.model;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class AdapterCDATA extends XmlAdapter<String, String> {
+
+  @Override
+  public String unmarshal(String v) throws Exception {
+    return v;
+  }
+
+  @Override
+  public String marshal(String v) throws Exception {
+    return "<![CDATA[" + v + "]]>";
+  }
+}
+hello
+Hollo
